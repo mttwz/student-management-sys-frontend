07002d7d2da5e968f41ba7989abd514952ad3cd2
mttwz <mateevoros@gmail.com>
2023-02-12 19:08:41 +0100

    modal

--- student-mgmnt-frontend/src/app/components/modals/superadmin-modal/superadmin-modal.component.ts
+++ student-mgmnt-frontend/src/app/components/modals/superadmin-modal/superadmin-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user/user.service';
+declare var $: any;  
+
 
 @Component({
   selector: 'app-superadmin-modal',
@@ -10,16 +12,27 @@ import { UserService } from 'src/app/services/user/user.service';
 export class SuperadminModalComponent implements OnInit {
 
   @Input() modalId!:String;
-  public addUserFormForm !: FormGroup;
+  public addUserForm !: FormGroup;
+  public userInfoForm !: FormGroup;
   public isSuccessful: any;
   public resStatus!: Number;
 
   constructor(private formBuilder: FormBuilder,private userService: UserService) { }
-
+  
   ngOnInit(): void {
-    
-    this.isSuccessful = null;
-    this.addUserFormForm = this.formBuilder.group({
+    $('#mainModal').on('hidden.bs.modal',  () => {
+      this.resStatus = 0;
+    })
+    this.addUserForm = this.formBuilder.group({
+      firstName: ['',Validators.required],
+      lastName: ['',Validators.required],
+      phone: ['',Validators.required],
+      birth: ['',Validators.required],
+      email: ['',Validators.required],
+      password: ['',Validators.required],
+      role:['',Validators.required]
+    })
+    this.userInfoForm = this.formBuilder.group({
       firstName: ['',Validators.required],
       lastName: ['',Validators.required],
       phone: ['',Validators.required],
@@ -30,30 +43,27 @@ export class SuperadminModalComponent implements OnInit {
     })
   }
 
-  submit(myForm: FormGroup)
-  {
-        console.log(myForm);
-  }
+
 
   addUser(myForm:FormGroup){
     
     console.log(myForm)
     this.userService.addUser(myForm.value).subscribe(res=>{
-     
-      
       myForm.reset();
+      console.log(res.status)
       this.isSuccessful = true;
       this.resStatus = res.status;
       
       
     },err=>{
       this.resStatus = err.status;
-     
       this.isSuccessful = false;
-      
-      
-      
     });
+
+    
   }
+
+  
+
 
 }
