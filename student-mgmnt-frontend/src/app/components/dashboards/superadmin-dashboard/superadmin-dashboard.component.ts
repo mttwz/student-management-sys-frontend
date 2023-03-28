8a93596b05f688c4de4c26390a2b0f1075fdd017
mttwz <75531518+mttwz@users.noreply.github.com>
2023-03-28 15:31:31 +0200

    table service2

--- student-mgmnt-frontend/src/app/components/dashboards/superadmin-dashboard/superadmin-dashboard.component.ts
+++ student-mgmnt-frontend/src/app/components/dashboards/superadmin-dashboard/superadmin-dashboard.component.ts
@@ -18,106 +18,30 @@ export class SuperadminDashboardComponent implements OnInit {
 
 
 
-  constructor(private userService: UserService, private authService: AuthService,private changeDetection: ChangeDetectorRef, private tableService: TableService) { 
+  constructor(private userService: UserService, public authService: AuthService,private changeDetection: ChangeDetectorRef, public tableService: TableService) { 
 
   
   }
 
-  checkedUsers: Array<Number> = [];
-  isLoading: Boolean = true;
-  isUsersLoading = true;
-  allUsers !: Array<any>;
-  isAnyChecked!: Boolean;
 
-  isOrderedById: Boolean = true;
-  isOrderedByRole: Boolean = false;
-  isOrderedByFirstName: Boolean = false;
-  isOrderedByLastName: Boolean = false;
-  isOrderedByEmail: Boolean = false;
-
-  groupName:string = "";
-
-  searchText:string = "";
-  searchFilter:string = "users";
-  pageSize:number = 100;
-  pageNumber:number = 0;
-  allPages!:number;
-  
-  sort:string = "id";
-  order:string = "asc";
 
   selectedModal!: String;
   isAnyModalActive: Boolean = false;
 
+
   @ViewChild(SuperadminModalComponent) SuperadminModalComponent!: SuperadminModalComponent;
   
   ngOnInit(): void {
-    this.searchAllUsers();
+    this.tableService.searchAllUsers();
 
     $(document).on('hidden.bs.modal','#mainModal',  () => {
-      
-      this.searchAllUsers();
-     
+      this.tableService.searchAllUsers();
       this.SuperadminModalComponent.isEditingEnabled = false;
-      
-
       
     })
     this.modalFix();
 
   }
-
-
-
-
-
-  logOut() {
-    this.authService.logOut();
-  }
-
-
-
-
-
-
-  getAllCheckedBox() {
-    this.checkedUsers = [];
-    let checkboxList = document.getElementsByClassName("subCheckbox");
-    for (let i = 0; i < checkboxList.length; i++) {
-      let currentCheckbox = checkboxList[i] as HTMLInputElement;
-      if (currentCheckbox.checked == true) {
-        this.checkedUsers.push(this.allUsers[i].id);
-      }
-    }
-    console.log(this.checkedUsers);
-  }
-
-  isAnyCheckedBox() {
-    let checkboxList = document.getElementsByClassName("subCheckbox");
-    for (let i = 0; i < checkboxList.length; i++) {
-      let currentCheckbox = checkboxList[i] as HTMLInputElement;
-      if (currentCheckbox.checked == true) {
-        this.isAnyChecked = true;
-        return;
-      }
-    }
-    this.isAnyChecked = false;
-  }
-
-  checkAllBox() {
-    let mainCheckbox = document.getElementById("mainCheckbox") as HTMLInputElement;
-
-    let checkboxList = document.getElementsByClassName("subCheckbox");
-    for (let i = 0; i < checkboxList.length; i++) {
-      let currentCheckbox = checkboxList[i] as HTMLInputElement;
-      if (mainCheckbox.checked == true) {
-        currentCheckbox.checked = true;
-      } else currentCheckbox.checked = false;
-
-    }
-    this.isAnyCheckedBox();
-  }
-
 
   modalFix() {
     let anoyingBackground = document.getElementsByClassName("modal-backdrop show")[0];
@@ -135,91 +59,12 @@ export class SuperadminDashboardComponent implements OnInit {
     }
   }
 
-  sortById(){
-    if(this.sort == 'id' && this.order == 'asc'){
-      this.sort='id';
-      this.order = 'desc'
-    }else if(this.sort == 'id' && this.order == 'desc'){
-      this.sort='id';
-      this.order = 'asc'
-    }
-    this.sort='id';
-    this.searchAllUsers();
-  }
-
-  sortByRole(){
-    if(this.sort == 'roleId.roleType' && this.order == 'asc'){
-      this.sort='roleId.roleType';
-      this.order = 'desc'
-    }else if(this.sort == 'roleId.roleType' && this.order == 'desc'){
-      this.sort='roleId.roleType';
-      this.order = 'asc'
-    }
-    this.sort='roleId.roleType';
-    this.searchAllUsers();
-  }
-
-  sortByFirstName(){
-    if(this.sort == 'firstName' && this.order == 'asc'){
-      this.sort='firstName';
-      this.order = 'desc'
-    }else if(this.sort == 'firstName' && this.order == 'desc'){
-      this.sort='firstName';
-      this.order = 'asc'
-    }
-    this.sort='firstName';
-    this.searchAllUsers();
-  }
-
-  sortByLastName(){
-    if(this.sort == 'lastName' && this.order == 'asc'){
-      this.sort='lastName';
-      this.order = 'desc'
-    }else if(this.sort == 'lastName' && this.order == 'desc'){
-      this.sort='lastName';
-      this.order = 'asc'
-    }
-    this.sort='lastName';
-    this.searchAllUsers();
-  }
-
-  sortByEmail(){
-    if(this.sort == 'email' && this.order == 'asc'){
-      this.sort='email';
-      this.order = 'desc'
-    }else if(this.sort == 'email' && this.order == 'desc'){
-      this.sort='email';
-      this.order = 'asc'
-    }
-    this.sort='email';
-    this.searchAllUsers();
-  }
-
-  searchAllUsers() {
-    
-    this.userService.searchSuperadmin(this.groupName,this.searchFilter,this.searchText,this.pageNumber,this.pageSize,this.sort,this.order).subscribe(res =>{
-      this.allUsers = res.userInfoDtoList;
-      this.isUsersLoading = false;
-      this.allPages = res.allPages;
-      this.changeDetection.detectChanges();
-      
-      console.log(this.allUsers);
-    },err=>{
-      console.log(err)
-    })
-  }
-
-  createRange(){
-    return new Array(this.allPages).fill(0)
-      .map((n, index) => index + 1);
-  }
-
-  pageClick(num: number){
-    this.pageNumber = num;
-    this.searchAllUsers();
 
 
-  }
+
+
+
+
 
 
 
--- student-mgmnt-frontend/src/app/services/table/table.service.ts
+++ student-mgmnt-frontend/src/app/services/table/table.service.ts
@@ -23,17 +23,89 @@ export class TableService {
   constructor(private userService: UserService) { }
 
   searchAllUsers() {
-
     this.userService.searchSuperadmin(this.groupName,this.searchFilter,this.searchText,this.pageNumber,this.pageSize,this.sort,this.order).subscribe(res =>{
       this.allUsers = res.userInfoDtoList;
       this.isUsersLoading = false;
       this.allPages = res.allPages;
-
-      
+      console.log(this.isUsersLoading)
       console.log(this.allUsers);
     },err=>{
       console.log(err)
     })
+  }
+
+  sortById(){
+    if(this.sort == 'id' && this.order == 'asc'){
+      this.sort='id';
+      this.order = 'desc'
+    }else if(this.sort == 'id' && this.order == 'desc'){
+      this.sort='id';
+      this.order = 'asc'
+    }
+    this.sort='id';
+    this.searchAllUsers();
+  }
+
+  sortByRole(){
+    if(this.sort == 'roleId.roleType' && this.order == 'asc'){
+      this.sort='roleId.roleType';
+      this.order = 'desc'
+    }else if(this.sort == 'roleId.roleType' && this.order == 'desc'){
+      this.sort='roleId.roleType';
+      this.order = 'asc'
+    }
+    this.sort='roleId.roleType';
+    this.searchAllUsers();
+  }
+
+  sortByFirstName(){
+    if(this.sort == 'firstName' && this.order == 'asc'){
+      this.sort='firstName';
+      this.order = 'desc'
+    }else if(this.sort == 'firstName' && this.order == 'desc'){
+      this.sort='firstName';
+      this.order = 'asc'
+    }
+    this.sort='firstName';
+    this.searchAllUsers();
+  }
+
+  sortByLastName(){
+    if(this.sort == 'lastName' && this.order == 'asc'){
+      this.sort='lastName';
+      this.order = 'desc'
+    }else if(this.sort == 'lastName' && this.order == 'desc'){
+      this.sort='lastName';
+      this.order = 'asc'
+    }
+    this.sort='lastName';
+    this.searchAllUsers();
+  }
+
+  sortByEmail(){
+    if(this.sort == 'email' && this.order == 'asc'){
+      this.sort='email';
+      this.order = 'desc'
+    }else if(this.sort == 'email' && this.order == 'desc'){
+      this.sort='email';
+      this.order = 'asc'
+    }
+    this.sort='email';
+    this.searchAllUsers();
+  }
+
+  pageClick(num: number){
+    this.pageNumber = num;
+    this.searchAllUsers();
+
 
   }
+
+  createRange(){
+    return new Array(this.allPages).fill(0)
+      .map((n, index) => index + 1);
+  }
+
+
+
 }
