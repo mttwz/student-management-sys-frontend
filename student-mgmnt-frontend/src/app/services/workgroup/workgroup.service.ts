73c2276d1d6850bc16da6e50c3c6c0fb8b71a8eb
lillarado <105851345+lillarado@users.noreply.github.com>
2023-04-03 11:21:54 +0200

    workgroup endpoints

--- student-mgmnt-frontend/src/app/services/workgroup/workgroup.service.ts
+++ student-mgmnt-frontend/src/app/services/workgroup/workgroup.service.ts
@@ -33,6 +33,14 @@ export class WorkgroupService {
   }
 
 
+  //Endpointok:
+
+  // Attendance:
+  // http://localhost:8080/api/v1/attendance/get-daily-attendance-by-user-id/
+  // http://localhost:8080/api/v1/attendance/get-attendance-by-student/2?page=0&size=100&sort=id,desc
+  // http://localhost:8080/api/v1/attendance/get-attendance-by-user/2?page=0&size=100&sort=id,desc
+
+
   getAllWorkgroup(pageNumber:number,pageSize:number,sort:string,order:string){
     return this.http.get<any>(environment.apiEndpoint + "/workgroup/get-all-workgroups?"+"&page="+pageNumber+"&size="+pageSize+"&sort="+sort+","+order);
   }
@@ -47,6 +55,26 @@ export class WorkgroupService {
 
   searchAllWorkgroupMemebers(groupName:string, q: string,pageNumber:number,pageSize:number,sort:string,order:string){
     return this.http.post<any>(environment.apiEndpoint + "/user/search-super-admin?category=users-in-workgroup"+"&q="+q+"&page="+pageNumber+"&size="+pageSize+"&sort="+sort+","+order,groupName);
+  }
+
+  addUserToWorkgroup(body:any){
+    // http://localhost:8080/api/v1/workgroup/add-user-to-workgroup
+    return this.http.post<any>(environment.apiEndpoint + "/workgroup/add-user-to-workgroup", body);
+  }
+
+  getUserSchedule(pageNumber:number, pageSize:number, sort:string, order:string){
+      // http://localhost:8080/api/v1/workgroupschedule/get-user-schedule/?page=0&size=2&sort=start,asc
+      return this.http.get<any>(environment.apiEndpoint + "/workgroupschedule/get-user-schedule/?page=" + pageNumber + "&size=" + pageSize + "&sort=" + sort + "," + order);
+  }
+
+  getWorkgroupScheduleByUserId(userId:number, pageNumber:number, pageSize:number,){
+      // http://localhost:8080/api/v1/workgroupschedule/get-workgroup-schedule-by-user-id/2?page=0&size=1
+      return this.http.get<any>(environment.apiEndpoint + "/workgroupschedule/get-workgroup-schedule-by-user-id/" + userId + "?page=" + pageNumber + "&size=" + pageSize);
+  }
+
+  getWorkgroupScheduleByWorkgroupId(workgroupId:number){
+      // http://localhost:8080/api/v1/workgroupschedule/get-workgroup-schedule-by-workgroup-id/1
+      return this.http.get<any>(environment.apiEndpoint + "/workgroupschedule/get-workgroup-schedule-by-workgroup-id/" + workgroupId);
   }
 
 }
